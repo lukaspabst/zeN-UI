b94de45d461def7611f934d5f8d314b9233fdd47
Lukas <lukas.pabst@outlook.de>
2026-01-20 00:46:44 +0100

    feat: Conditionally disable the DTS plugin for Storybook builds.

--- vite.config.ts
+++ vite.config.ts
@@ -2,18 +2,20 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 import dts from 'vite-plugin-dts';
 
-export default defineConfig({
-    build: {
-        lib: {
-            entry: resolve(__dirname, 'src/lib/index.ts'),
-            name: 'ZenUI',
-            fileName: 'personal-library',
+export default defineConfig(({ command, mode }) => {
+    const isStorybookBuild = process.argv.some(arg => arg.includes('storybook'));
+
+    return {
+        build: {
+            lib: {
+                entry: resolve(__dirname, 'src/lib/index.ts'),
+                name: 'ZenUI',
+                fileName: 'personal-library',
+            },
+            rollupOptions: {
+                external: [/^lit/],
+            },
         },
-        rollupOptions: {
-            external: [/^lit/],
-        },
-    },
-    plugins: [
-        dts({ rollupTypes: true })
-    ],
+        plugins: isStorybookBuild ? [] : [dts({ rollupTypes: true })],
+    };
 });
